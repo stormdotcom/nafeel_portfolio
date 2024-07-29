55c2be2a0ea00996786095f420eabba06de30cc6
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-07-29 13:12:43 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
                 </div>
               )
             })}
-          </div >
+          </div>
         </div>
       </Container>
       <Container>
